884743edc4dd07e6ceb1e10dbddf52b61455d832
JohnScott <johnscott.jiang@gmail.com>
2022-10-27 14:17:20 +0800

    更符合要求的

--- 03/App.java
+++ 03/App.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.*;
 
 import scr.StuGroup;
 import scr.Student;
@@ -8,19 +9,31 @@ import scr.Student;
 public class App {
     public static void main(String[] args) throws Exception {
         StuGroup group = new StuGroup(20);
-        File fl = new File("info.txt");
-        BufferedReader reader = new BufferedReader(new FileReader(fl));
-        String stdinf = new String();
-        while ((stdinf = reader.readLine()) != null){
-            String[] info = stdinf.split(" ");
-            group.addStu(new Student(info[0],
-                                     info[1],
-                                     info[2].charAt(0),
-                                     Integer.parseInt(info[3]),
-                                     Integer.parseInt(info[4]),
-                                     Integer.parseInt(info[5])));
+        group.load("info.txt");
+        int num;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("需要输入几个学生：");
+        num = reader.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.println("请输入第" + (i + 1) + "个学生的姓名、地址、性别、年龄、数学、英语信息：");
+            group.addStu(new Student(
+                    reader.next(),
+                    reader.next(),
+                    reader.next().charAt(0),
+                    reader.nextInt(),
+                    reader.nextInt(),
+                    reader.nextInt()));
         }
-        reader.close();
+        System.out.print("需要删除几个学生：");
+        num = reader.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.print("删除第几个学生：");
+            group.removeStu(reader.nextInt());
+            System.out.print("第" + i + "个学生已经删除。");
+        }
+        System.out.println("最新的学生信息是：");
         group.printelem();
+        reader.close();
+        group.write("info.txt");
     }
 }
--- 03/scr/StuGroup.java
+++ 03/scr/StuGroup.java
@@ -1,4 +1,10 @@
 package scr;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+
 public class StuGroup implements Mygroup {
     private Student[] stu;
     private int length;
@@ -39,10 +45,41 @@ public class StuGroup implements Mygroup {
     }
 
     public void printelem() {
-        for (int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             System.out.println("---------------");
             stu[i].printelem();
             System.out.println("---------------");
         }
+    }
+
+    public void load(String x) throws Exception {
+        File fr = new File(x);
+        BufferedReader reader = new BufferedReader(new FileReader(fr));
+        String stdinf = new String();
+        while ((stdinf = reader.readLine()) != null) {
+            String[] info = stdinf.split(" ");
+            this.addStu(new Student(
+                    info[0],
+                    info[1],
+                    info[2].charAt(0),
+                    Integer.parseInt(info[3]),
+                    Integer.parseInt(info[4]),
+                    Integer.parseInt(info[5])));
+        }
+        reader.close();
+    }
+
+    public void write(String x) throws Exception {
+        File fw = new File(x);
+        FileWriter writer = new FileWriter(fw);
+        String str = new String();
+        for (int i = 0; i < this.length; i++) {
+            str = stu[i].name + " " + stu[i].addr + " " + stu[i].sex + " " + stu[i].age + " " + stu[i].math + " "
+                    + stu[i].eng;
+            writer.write(str);
+            if (i != this.length - 1)
+                writer.write("\r\n");
+        }
+        writer.close();
     }
 }
