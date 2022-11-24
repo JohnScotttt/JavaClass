9fbdf5029446c556285a165dbafc9d3b1154cb94
JohnScott <johnscott.jiang@gmail.com>
2022-11-24 08:51:35 +0800

    什么勾八作业啊，五子棋！

--- 06/src/ChessJFrame.java
+++ 06/src/ChessJFrame.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.util.Base64;
 
 public class ChessJFrame extends JFrame {
     private int second = 0;
